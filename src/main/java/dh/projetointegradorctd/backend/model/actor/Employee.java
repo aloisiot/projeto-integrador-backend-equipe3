1f9c9f85bd95e8fdc12e7ba41bff7b841f0c6d7a
Aloísio Carvalho <loistc.carvalho@gmail.com>
2022-03-26 15:38:39 -0300

    implementando entidade Employee

---
+++ src/main/java/dh/projetointegradorctd/backend/model/actor/Employee.java
@@ -0,0 +1,9 @@
+package dh.projetointegradorctd.backend.model.actor;
+
+import dh.projetointegradorctd.backend.model.auth.User;
+
+import javax.persistence.Entity;
+
+@Entity
+public class Employee extends User {
+}
