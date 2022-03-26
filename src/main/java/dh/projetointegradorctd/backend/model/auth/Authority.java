9d114d89361cd70368d7c81e04f3193646bf84c7
Aloísio Carvalho <loistc.carvalho@gmail.com>
2022-03-26 15:39:30 -0300

    refatoralção

--- src/main/java/dh/projetointegradorctd/backend/model/auth/Authority.java
+++
@@ -1,5 +0,0 @@
-package dh.projetointegradorctd.backend.model.auth;
-
-public enum Authority {
-    ADMIN, CLIENT
-}
---
+++ src/main/java/dh/projetointegradorctd/backend/model/auth/Role.java
@@ -0,0 +1,27 @@
+package dh.projetointegradorctd.backend.model.auth;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+
+@Getter
+@Setter
+@Embeddable
+public class Role implements GrantedAuthority {
+
+    @NotNull
+    @Column(length = 20)
+    @Enumerated(EnumType.STRING)
+    private Role.Authority authority;
+
+    public String getAuthority() {
+        return this.authority.name();
+    }
+
+    public enum Authority {
+        ADMIN, CLIENT
+    }
+}
