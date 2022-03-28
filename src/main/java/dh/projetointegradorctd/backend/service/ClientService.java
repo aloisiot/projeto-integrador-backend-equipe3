3feeeb2c959ac98568a84e974bf07a3f8dc95bea
Aloísio Carvalho <loistc.carvalho@gmail.com>
2022-03-28 16:18:23 -0300

    implementaçao de serviçoes de produtos favoritados

--- src/main/java/dh/projetointegradorctd/backend/service/ClientService.java
+++ src/main/java/dh/projetointegradorctd/backend/service/ClientService.java
@@ -1,11 +1,16 @@
 package dh.projetointegradorctd.backend.service;
 
+import dh.projetointegradorctd.backend.exception.global.ResorceNotFoundException;
 import dh.projetointegradorctd.backend.exception.global.UnprocessableEntityException;
 import dh.projetointegradorctd.backend.model.actor.Client;
+import dh.projetointegradorctd.backend.model.storage.Product;
 import dh.projetointegradorctd.backend.repository.ClientRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class ClientService extends TemplateCrudService<Client> {
@@ -22,5 +27,13 @@ public class ClientService extends TemplateCrudService<Client> {
     public Client save(Client client) throws UnprocessableEntityException {
         client.setPassword(passwordEncoder.encode(client.getPassword()));
         return super.save(client);
+    }
+
+    public List<Product> findFavoritesByClientId(Long clientId) {
+        Optional<Client> client = repository.findById(clientId);
+        if(client.isEmpty() || client.get().getFavoriteProducts().isEmpty()){
+            throw new ResorceNotFoundException();
+        }
+        return client.get().getFavoriteProducts();
     }
 }
